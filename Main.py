3abb89d1371b182b720b08d963aa524d5bc2a0ae
Petros-13 <petroskalkounos78@gmail.com>
2025-08-07 15:25:31 +0200

    1st commit

---
+++ Main.py
@@ -0,0 +1,11 @@
+i=1
+while i<20:
+    print("The number is",i)
+    i+=1
+j=int(input("Enter a number:"))
+k=0
+sum=0
+while k<=j:
+    sum+=k
+    k+=1
+print("The sum of numbers from 1",j,"is",sum)
